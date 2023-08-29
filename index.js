a100a96db4131317ab880847285a8781e289e9a0
Ebrusike Favour <ebrusikefavour@gmail.com>
2023-08-29 09:28:51 -0700

    added address

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const port = 8080;
 const cors = require('cors')
 app.use(cors())
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const jwt = require("jsonwebtoken")
 
@@ -121,5 +121,36 @@ app.post('/login', async (req, res) => {
         res.status(200).json({token})
     } catch (error) {
         res.status(500).json({message:"Login failed"})
+    }
+})
+
+app.post("/address", async (req,res )=>{
+    try {
+        const { userId, address } = req.body;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({message:"User not found"})
+        }
+        user.addresses.push(address);
+        await user.save();
+        res.status(200).json({message:"Address created successfully "})
+    } catch (error) {
+        res.status(500).json({ message: "Error adding message" });
+    }
+})
+
+app.get("/addresses/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({message:"User not found"})
+        }
+
+        const addresses = user.addresses;
+        res.status(200).json({addresses})
+
+    } catch (error) {
+        res.status(500).json({message:"Error retrieving the addresses"})
     }
 })
