47e84b4b054ee43d683592ea21a417845ba11dcf
Ebrusike Favour <ebrusikefavour@gmail.com>
2023-08-31 19:36:30 -0700

    update order request

--- index.js
+++ index.js
@@ -157,31 +157,38 @@ app.get("/addresses/:userId", async (req, res) => {
 
 app.post('/orders', async (req, res) => {
     try {
-        const { userId, cartItems, totalPrice, shippingAddress, paymentMethod } = req.body;
-        const user = await User.findById(userId)
-        if (!user) {
-            return res.status(404).json({message:"User not found"})
-        }
-        const products = cartItems.map((item) => ({
-            name: item?.title,
-            quantity: item?.quantity,
-            price: item?.price,
-            image:item?.image
-        }))
-        
-        const order = new Order({
-            user: userId,
-            products: products,
-            totalPrice: totalPrice,
-            shippingAddress: shippingAddress,
-            paymentMethod: paymentMethod
-        })
-        await order.save();
-        res.status(200).json({message:"Order created successfully"})
-    } catch (error) {
-        console.log("error creating orders", error)
-        res.status(500).json({message:"Error creating orders"})
+    const { userId, cartItems, totalPrice, shippingAddress, paymentMethod } =
+      req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    //create an array of product objects from the cart Items
+    const products = cartItems.map((item) => ({
+      name: item?.title,
+      quantity: item.quantity,
+      price: item.price,
+      image: item?.image,
+    }));
+
+    //create a new Order
+    const order = new Order({
+      user: userId,
+      products: products,
+      totalPrice: totalPrice,
+      shippingAddress: shippingAddress,
+      paymentMethod: paymentMethod,
+    });
+
+    await order.save();
+
+    res.status(200).json({ message: "Order created successfully!" });
+  } catch (error) {
+    console.log("error creating orders", error);
+    res.status(500).json({ message: "Error creating orders" });
+  }
 })
 
 
